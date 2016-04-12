9f1585ae04b0d36701485e7a8a4d743d1afed8e7
Tyler Collier <git@tylercollier.com>
2016-04-12 12:56:01 -0700

    Add test for checking simulated DOM render

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -7,4 +7,7 @@ describe(Dashboard, () => {
   it('outputs the text hi', () => {
     expect(shallow(<Dashboard/>).text()).to.equal("hi")
   })
+  it('renders HTML', () => {
+    expect(shallow(<Dashboard/>).equals(<div>hi</div>)).to.equal(true)
+  })
 })
