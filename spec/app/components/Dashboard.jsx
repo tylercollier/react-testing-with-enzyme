5b1e0905b2a23c06a3ed42ddbaadc9b873538547
Tyler Collier <git@tylercollier.com>
2016-04-13 12:06:19 -0700

    Only spy test, simple

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -1,10 +1,6 @@
 import chaiEnzyme from 'chai-enzyme'
 import React from 'react'
 import Dashboard from '../../../app/components/Dashboard'
-import QuoteCollection from '../../../app/components/QuoteCollection'
-import Quote from '../../../app/components/Quote'
-import PhraseCollection from '../../../app/components/PhraseCollection'
-import Phrase from '../../../app/components/Phrase'
 import TestUtils from 'react-addons-test-utils'
 import _quotes from '../../../fixtures/quotes'
 import sinon from 'sinon'
@@ -13,19 +9,6 @@ chai.use(chaiEnzyme())
 
 describe('Dashboard', () => {
   context("with Facebook's test utils", () => {
-    it('renders a full section of DOM', () => {
-      const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes}/>)
-      const quoteCollection = TestUtils.scryRenderedComponentsWithType(dashboard, QuoteCollection)
-      expect(quoteCollection).to.have.length.of(1)
-      const quotes = TestUtils.scryRenderedComponentsWithType(quoteCollection[0], Quote)
-      expect(quotes).to.have.length.of(3)
-      const phraseCollection = TestUtils.scryRenderedComponentsWithType(quotes[2], PhraseCollection)
-      expect(phraseCollection).to.have.length.of(1)
-      const phrases = TestUtils.scryRenderedComponentsWithType(phraseCollection[0], Phrase)
-      expect(phrases).to.have.length.of(2)
-      expect(phrases[0].props.phrase.text).to.equal("Everyone has an irrational fear")
-    })
-
     it("handles clicking 'Create new quote' button", () => {
       const handleCreateLink = sinon.spy()
       const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes} onCreateLink={handleCreateLink}/>)
