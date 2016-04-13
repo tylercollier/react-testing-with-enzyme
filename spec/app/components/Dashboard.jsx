23d2ba40ded0b6218070d5d4954591a4eb7560de
Tyler Collier <git@tylercollier.com>
2016-04-13 16:07:54 -0700

    Enzyme mount deep

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -2,20 +2,28 @@ import chaiEnzyme from 'chai-enzyme'
 import React from 'react'
 import Dashboard from '../../../app/components/Dashboard'
 import QuoteCollection from '../../../app/components/QuoteCollection'
+import Quote from '../../../app/components/Quote'
 import PhraseCollection from '../../../app/components/PhraseCollection'
+import Phrase from '../../../app/components/Phrase'
 import TestUtils from 'react-addons-test-utils'
 import _quotes from '../../../fixtures/quotes'
+import { shallow, mount } from 'enzyme'
 
 chai.use(chaiEnzyme())
 
 describe(Dashboard, () => {
-  context("with Facebook's test utils", () => {
+  context("with enzyme", () => {
     it('renders a full section of DOM', () => {
-      const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes}/>)
-      const quoteCollection = TestUtils.scryRenderedComponentsWithType(dashboard, QuoteCollection);
+      const dashboard = mount(<Dashboard quotes={_quotes}/>)
+      const quoteCollection = dashboard.find(QuoteCollection)
       expect(quoteCollection).to.have.length.of(1)
-      const phraseCollection = TestUtils.scryRenderedComponentsWithType(dashboard, PhraseCollection);
-      expect(phraseCollection).to.have.length.of(3)
+      const quotes = quoteCollection.at(0).find(Quote)
+      expect(quotes).to.have.length.of(3)
+      const phraseCollection = quotes.at(2).find(PhraseCollection)
+      expect(phraseCollection).to.have.length.of(1)
+      const phrases = phraseCollection.at(0).find(Phrase)
+      expect(phrases).to.have.length.of(2)
+      expect(phrases.at(0).props().phrase.text).to.equal("Everyone has an irrational fear")
     })
   })
 })
