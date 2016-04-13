317a9437cce8544d6cf5a37211efebf618f8c725
Tyler Collier <git@tylercollier.com>
2016-04-13 12:54:11 -0700

    Only renderIntoDocument, simple

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -2,12 +2,8 @@ import chaiEnzyme from 'chai-enzyme'
 import React from 'react'
 import Dashboard from '../../../app/components/Dashboard'
 import QuoteCollection from '../../../app/components/QuoteCollection'
-import Quote from '../../../app/components/Quote'
-import PhraseCollection from '../../../app/components/PhraseCollection'
-import Phrase from '../../../app/components/Phrase'
 import TestUtils from 'react-addons-test-utils'
 import _quotes from '../../../fixtures/quotes'
-import sinon from 'sinon'
 
 chai.use(chaiEnzyme())
 
@@ -17,13 +13,6 @@ describe('Dashboard', () => {
       const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes}/>)
       const quoteCollection = TestUtils.scryRenderedComponentsWithType(dashboard, QuoteCollection)
       expect(quoteCollection).to.have.length.of(1)
-      const quotes = TestUtils.scryRenderedComponentsWithType(quoteCollection[0], Quote)
-      expect(quotes).to.have.length.of(3)
-      const phraseCollection = TestUtils.scryRenderedComponentsWithType(quotes[2], PhraseCollection)
-      expect(phraseCollection).to.have.length.of(1)
-      const phrases = TestUtils.scryRenderedComponentsWithType(phraseCollection[0], Phrase)
-      expect(phrases).to.have.length.of(2)
-      expect(phrases[0].props.phrase.text).to.equal("Everyone has an irrational fear")
     })
   })
 })
