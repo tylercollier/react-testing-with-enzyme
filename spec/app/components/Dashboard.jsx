5cb6afcbd2a223e5615fb5bea4813d5285fb7a13
Tyler Collier <git@tylercollier.com>
2016-04-13 11:57:53 -0700

    Only spy test for deep component render

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -13,26 +13,6 @@ chai.use(chaiEnzyme())
 
 describe('Dashboard', () => {
   context("with Facebook's test utils", () => {
-    it('renders a full section of DOM', () => {
-      const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes}/>)
-      const quoteCollection = TestUtils.scryRenderedComponentsWithType(dashboard, QuoteCollection)
-      expect(quoteCollection).to.have.length.of(1)
-      const quotes = TestUtils.scryRenderedComponentsWithType(quoteCollection[0], Quote)
-      expect(quotes).to.have.length.of(3)
-      const phraseCollection = TestUtils.scryRenderedComponentsWithType(quotes[2], PhraseCollection)
-      expect(phraseCollection).to.have.length.of(1)
-      const phrases = TestUtils.scryRenderedComponentsWithType(phraseCollection[0], Phrase)
-      expect(phrases).to.have.length.of(2)
-      expect(phrases[0].props.phrase.text).to.equal("Everyone has an irrational fear")
-    })
-
-    it("handles clicking 'Create new quote' button", () => {
-      const handleCreateLink = sinon.spy()
-      const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes} onCreateLink={handleCreateLink}/>)
-      TestUtils.Simulate.click(dashboard.refs.createQuoteLink)
-      expect(handleCreateLink).to.have.been.called
-    })
-
     it("handles clicking 'Like'", () => {
       const handleLikeQuote = sinon.spy()
       const dashboard = TestUtils.renderIntoDocument(<Dashboard quotes={_quotes} onLikeQuote={handleLikeQuote}/>)
