1909041f33bf1c900a3a1ffcecdf416fbec9d0c9
Tyler Collier <git@tylercollier.com>
2016-04-12 17:35:04 -0700

    Add ReactTestUtils tests for comparison

--- spec/app/components/Dashboard.jsx
+++ spec/app/components/Dashboard.jsx
@@ -10,19 +10,59 @@ import Dashboard from '../../../app/components/Dashboard'
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import QuoteList from '../../../app/components/QuoteList'
+import ReactTestUtils from 'react-addons-test-utils'
 
 chai.use(chaiEnzyme())
 
 describe(Dashboard, () => {
-  it("outputs the text 'Here are the most recent quotes'", () => {
-    // The following gives me a runtime error. I suspect it's a bug in chai-enzyme. So I use 'match' as a workaround.
-    // expect(shallow(<Dashboard/>)).to.contain.text("Here are the most recent quotes")
-    expect(shallow(<Dashboard/>)).to.have.text().match(/Here are the most recent quotes/)
+  context('with enzyme', () => {
+    it("outputs the text 'Here are the most recent quotes'", () => {
+      // The following gives me a runtime error. I suspect it's a bug in chai-enzyme. So I use 'match' as a workaround.
+      // expect(shallow(<Dashboard/>)).to.contain.text("Here are the most recent quotes")
+      expect(shallow(<Dashboard/>)).to.have.text().match(/Here are the most recent quotes/)
+    })
+
+    it('renders HTML', () => {
+      expect(shallow(<Dashboard/>)).to.contain(<p>Here are the most recent quotes</p>)
+    })
+
+    it('mounts and can find an internal component', () => {
+      expect(mount(<Dashboard/>)).to.have.exactly(1).descendants(QuoteList)
+    })
   })
-  it('renders HTML', () => {
-    expect(shallow(<Dashboard/>)).to.contain(<p>Here are the most recent quotes</p>)
-  })
-  it('mounts and can find an internal component', () => {
-    expect(mount(<Dashboard/>)).to.have.exactly(1).descendants(QuoteList)
+
+  context("with Facebook's test utils", () => {
+    it("outputs the text 'Here are the most recent quotes'", () => {
+      const shallowRenderer = ReactTestUtils.createRenderer()
+      shallowRenderer.render(<Dashboard/>)
+      const result = shallowRenderer.getRenderOutput()
+      expect(result.props.children[0].props.children).to.equal('Here are the most recent quotes')
+    })
+
+    it('renders HTML', () => {
+      const shallowRenderer = ReactTestUtils.createRenderer()
+      shallowRenderer.render(<Dashboard/>)
+      const result = shallowRenderer.getRenderOutput()
+      // Here we can check the whole thing.
+      // We must use 'deep.equal' or 'eql' because we want to compare value, not identity.
+      expect(result).to.deep.equal(
+        <div>
+          <p>Here are the most recent quotes</p>
+          <QuoteList/>
+        </div>
+      )
+      // But there are no built in utils/helpers for "contains", so we could do something like this to mimic the above test.
+      // This is more brittle than a 'contains' assertion, because we our check here considers the ORDER of the children.
+      expect(result.props.children[0].type).to.equal('p')
+      expect(result.props.children[0]).to.deep.equal(
+        <p>Here are the most recent quotes</p>
+      )
+    })
+
+    it('mounts and can find an internal component', () => {
+      const dashboard = ReactTestUtils.renderIntoDocument(<Dashboard/>)
+      const quoteList = ReactTestUtils.scryRenderedComponentsWithType(dashboard, QuoteList);
+      expect(quoteList).to.have.length.of(1)
+    })
   })
 })
